c4ec84aecf9e3892a9dfa9dbb094da2a33091c09
Prajwal Goswami <prajwalgoswami1996@gmail.com>
2019-01-20 13:45:47 +0530

    challenge import export

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import myCurrentLocation, { message, name } from './myModule';
+import add, { sub } from './maths';
 
 console.log(message);
 console.log(name);
 console.log(myCurrentLocation);
+
+console.log(add(12,14));
+console.log(sub(16,14));
---
+++ src/maths.js
@@ -0,0 +1,8 @@
+const add = (x,y) => {
+    return x+y;
+}
+const sub = (x,y) => {
+    return x-y;
+}
+
+export { sub, add as default}
