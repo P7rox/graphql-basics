baabd1c5919b5f932f9cb15bfb1feb4d784cf986
Prajwal Goswami <prajwalgoswami1996@gmail.com>
2019-01-20 13:36:50 +0530

    part 1

---
+++ src/myModule.js
@@ -0,0 +1,8 @@
+// Named exports. multiple possible
+// Default export. only one
+const name = "P7";
+const message = "some message";
+
+const location = "Land of kings";
+
+export { message, name, location as default }
